33ea0cff089b0401204123ee5ce9c0868e56d80d
Usman Adam Imam <uadam12@gmail.com>
2024-12-12 13:51:16 +0100

    use postgrs

--- app/settings.py
+++ app/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': env('DATABASE_NAME'),
-#        'USER': env('DATABASE_USER'),
-#        'PASSWORD': env('DATABASE_PASS'),
-#        'HOST': env('DATABASE_HOST'),
-#        'PORT': env('DATABASE_PORT'),
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
